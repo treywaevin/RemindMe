22c5da96bef40f7c6b63b7b9f73d1816fa9624bc
Aevin Eliares <aevin.eliares@gmail.com>
2022-08-27 00:00:27 -0700

    added basic files

--- bot.py
+++ bot.py
@@ -2,15 +2,28 @@
 
 # Import Discord API
 import discord
+from discord.ext import commands
 
-TOKEN = 'MTAxMjU3NzgwNDMzODA5MDAwNQ.GbEPvB.Dium6fMViGBtplen4FSpTBl9HasKfyJ18bTGPU'
+TOKEN = 'MTAxMjU3NzgwNDMzODA5MDAwNQ.GAU-D0.NUOenrKIB41npiu1lJ4UxNvkOdswS0GvD5H2Gw'
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+
 
 @client.event
 async def on_ready():
     # Bot has been properly initialized
     print("{0.user} has been initialized".format(client))
 
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
 # Runs Bot
 client.run(TOKEN) 
--- reminder.py
+++ reminder.py
@@ -1,1 +1,46 @@
 # This file contains all functions related to the reminder aspect of the bot
+
+# Import Modules for database
+from array import array
+import json
+from collections import defaultdict
+
+# Dictionary that stores all assignments by subject
+asgns = defaultdict(list)
+
+# Assignment Object
+class Assignment():
+    def __init__(self, subject, name, due_date):
+        self.subject = subject
+        self.name = name
+        self.due_date = due_date
+
+    def change_due_date(self,new_date):
+        self.due_date = new_date
+    
+    def change_name(self, new_name):
+        self.name = new_name
+
+
+
+def add_assignment(subject, name, due_date):
+    # Create new assignment
+    new_asgn = Assignment(subject,name,due_date)
+
+    # Add assignment to dictionary
+    asgns[new_asgn.subject].append(new_asgn)
+
+# Prints List of subjects
+def list_subjects():
+    for i in asgns:
+        print(i)
+
+
+# Testing Purposes
+add_assignment("CSE101", "PA 1", "10/17/1999")
+add_assignment("CSE13S", "PA 2", "10/17/1999")
+print(asgns)
+list_subjects()
+
+
+
